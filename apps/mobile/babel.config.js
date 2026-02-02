435807c798ffe1feea597c754f85077614dc7b44
E. Kofler <eliaskofler@icloud.com>
2026-02-02 22:43:32 +0100

    u

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,4 +1,4 @@
-module.exports = function(api) {
+module.exports = function (api) {
     api.cache(true);
     return {
         presets: ['babel-preset-expo'],
@@ -6,9 +6,8 @@ module.exports = function(api) {
             [
                 'module-resolver',
                 {
-                    root: ['./*'],
                     alias: {
-                        '@': './*', // <-- points @/ to apps/mobile/app
+                        '@': '.',
                     },
                 },
             ],
